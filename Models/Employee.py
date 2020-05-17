3472ddfc829969853aae2e0a1acc5ce2717d07e2
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-05-17 00:19:25 -0400

    Create employee model

---
+++ Models/Employee.py
@@ -0,0 +1,18 @@
+# Employee Class
+#
+# Author: Alejandro Penaloza
+# Last Updated: May 16 2020
+
+""" Employee Model """
+class Employee:
+
+    # creates an instance of an Employee
+    def __init__(self, firstName, lastName, dept, salary):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.dept = dept
+        self.salary = salary
+        self.email = firstName + '.' + lastName + '@apenaloza.com'
+
+    def fullName(self):
+        return '{} {}'.format(self.firstName, self.lastName)
