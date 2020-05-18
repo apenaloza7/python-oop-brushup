6dcc5e237325542a4067ffca0b8a60f605a15d25
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-05-18 01:25:37 -0400

    Added remove,add employees method

--- Models/Manager.py
+++ Models/Manager.py
@@ -1,4 +1,4 @@
-from Models.Employee import *
+from .Employee import *
 
 # Manager Model
 #
@@ -7,11 +7,39 @@ from Models.Employee import *
 #
 # Sub-class of Employee model
 
-def manager(Employee):
+class Manager(Employee):
 
-     annual_raise_amount = 1.14
+    annual_raise_amount = 1.14
 
-     # creates instance of a manager
-     def __init__(self, firstName, lastName, dept, salary, deptManaged):
-         super().__init__(firstName, lastName, dept, salary)
-         self.deptManaged = deptManaged
+    # creates instance of a manager
+    def __init__(self, firstName, lastName, dept, salary, deptManaged, employees = None):
+        super().__init__(firstName, lastName, dept, salary)
+        self.deptManaged = deptManaged
+
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    # allows for the addition of managed employees
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    # allows for the removal of managed employees
+    def remove_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    # prints the employees onto the terminal
+    def print_employees(self):
+
+        # counter for the displaying of employees
+        employee_counter = 1
+
+        for emp in self.employees:
+            print(str(employee_counter) + ' '  + emp.fullName() + '\n')
+            employee_counter += 1
+
+        # resets the counter back to one after the for-loop is over
+        employee_counter = 1
