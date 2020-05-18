82bdd09f92a2dd9243f03e2c5065de43a9fc89a1
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-05-18 02:13:50 -0400

    update test.py

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ from Models.Manager import *
 
 # Test file used to test my methods as I go along
 
-emp_1 = Employee('Alejandro', 'Penaloza', 'Software', 100000)
-manager_1 = Manager('Alex', 'Penaloza', 'Software', 120000, 'Software', [emp_1])
+emp_000 = Employee('Alejandro', 'Penaloza', 'Software', 100000)
 
-print(manager_1.annual_raise_amount)
-manager_1.add_employee(emp_1)
-print(manager_1.print_employees())
+print(emp_000.firstName)
+
+# manager_000 = Manager('Alex', 'Penaloza', 'Software', 120000, 'Software', [emp_000])
+
+# manager_000.add_employee(emp_000)
+# manager_000.print_employees()
